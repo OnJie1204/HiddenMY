3e6e63bebf0277c01140c268480a6fac187c227c
OnJie1204 <oonweijie6399@gmail.com>
2026-07-14 12:48:40 +0800

    Revert "fix update profile bug"
    This reverts commit 2e1f2d08416e203f96466f4a3d877c1c9b1cb1f5.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,7 +51,7 @@ function App() {
           user ? <Layout user={user} setUser={setUser}><Maps /></Layout> : <Navigate to="/login" />
         } />
         <Route path="/profile" element={
-          user ? <Layout user={user} setUser={setUser}><Profile setAppUser={setUser} /></Layout> : <Navigate to="/login" />
+          user ? <Layout user={user} setUser={setUser}><Profile /></Layout> : <Navigate to="/login" />
         } />
       </Routes>
     </BrowserRouter>
