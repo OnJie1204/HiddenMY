41141245fbc77fc3472f85db0ab2b27701ef01fe
OnJie1204 <oonweijie6399@gmail.com>
2026-09-03 22:33:26 +0800

    Revert "fix map"
    This reverts commit e793f88dc6b0b4237b279f58b5a356ec08fb6939.

--- laravel/resources/js/api/gemInteractions.js
+++ laravel/resources/js/api/gemInteractions.js
@@ -1,5 +1,3 @@
 import api from "../api";
 
 export const getMyRatings = () => api.get("/my-ratings");
-
-export const getInteractions = (locationId) => api.get(`/gem-interactions/${locationId}`);
--- laravel/resources/js/components/SidePanel.jsx
+++ laravel/resources/js/components/SidePanel.jsx
@@ -651,11 +651,6 @@ function SidePanel({
                                                         })}
                                                     </span>
                                                 </div>
-                                                {review.rating > 0 && (
-                                                    <span className="side-panel-review-stars" aria-label={`${review.rating} out of 5 stars`}>
-                                                        {"★".repeat(review.rating)}{"☆".repeat(5 - review.rating)}
-                                                    </span>
-                                                )}
                                                 {review.photo_path && (
                                                     <img
                                                         src={getVotePhotoUrl(review.photo_path)}
@@ -664,8 +659,8 @@ function SidePanel({
                                                         onError={(e) => { e.target.style.display = "none"; }}
                                                     />
                                                 )}
-                                                {review.comment && (
-                                                    <p className="side-panel-review-text">"{review.comment}"</p>
+                                                {review.travel_description && (
+                                                    <p className="side-panel-review-text">"{review.travel_description}"</p>
                                                 )}
                                             </div>
                                         ))}
